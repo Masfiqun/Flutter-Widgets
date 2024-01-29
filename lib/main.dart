22d20d0c8e24e189f217fe3f402772bc7bda0883
Masfiqun <masfiqunahmednooneknows@gmail.com>
2024-01-29 19:50:28 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  MaterialApp build(BuildContext context) {
+    return MaterialApp(
+      home: Widget(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Widget extends StatefulWidget {
+  const Widget({super.key});
+
+  @override
+  State<Widget> createState() => _WidgetState();
+}
+
+class _WidgetState extends State<Widget> {
+  @override
+  SafeArea build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(50),
+            child: Image.network('https://picsum.photos/250?image=9'),
+          ),
+        )
+      )
+    );
+  }
+}
