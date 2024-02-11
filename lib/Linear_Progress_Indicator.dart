427444b4aa6297bd5ea2f7cffedbce2f183d0560
Masfiqun <masfiqunahmednooneknows@gmail.com>
2024-02-11 20:51:20 +0600

    linear progress indicator

--- lib/Linear_Progress_Indicator.dart
+++ lib/Linear_Progress_Indicator.dart
@@ -1,19 +1,19 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
 class Linear_Progress_Indicator extends StatefulWidget {
-  const Linear_Progress_Indicator({super.key});
+  const Linear_Progress_Indicator({Key? key}) : super(key: key);
 
   @override
   State<Linear_Progress_Indicator> createState() => _Linear_Progress_IndicatorState();
 }
 
-class _Linear_Progress_IndicatorState extends State<Linear_Progress_Indicator> {
+class _Linear_Progress_IndicatorState extends State<Linear_Progress_Indicator> with SingleTickerProviderStateMixin {
  
  late AnimationController controller;
+ 
  @override
- void initState(){
+ void initState() {
+  super.initState();
   controller = AnimationController(
     vsync: this,
     duration: const Duration(seconds: 5),
@@ -21,26 +21,30 @@ class _Linear_Progress_IndicatorState extends State<Linear_Progress_Indicator> {
     setState(() {});
   });
   controller.repeat(reverse: true);
-  super.initState();
  }
 
  @override
- void dispose(){
+ void dispose() {
   controller.dispose();
   super.dispose();
  }
  
- 
  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text('Linear_Progress_Indicator'),
-          LinearProgressIndicator(
-            value: controller.value,
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Linear_Progress_Indicator'),
+            LinearProgressIndicator(
+              backgroundColor: Colors.amberAccent,
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.tealAccent),
+              value: controller.value,
+            ),
+          ],
+        ),
       )
     );
   }
